4823a9dd62ab16f51dbbbe8d9d180e436e6f8dfe
pauloue <oue.paul18@gmail.com>
2020-04-16 02:09:27 +0200

    Add prettier group names in tooltip

--- init.lua
+++ init.lua
@@ -130,6 +130,41 @@ craftguide.group_stereotypes = {
 	mesecon_conductor_craftable = "mesecons:wire_00000000_off",
 }
 
+local GROUP_NAMES = {
+	coal = S"Any coal",
+	wool = S"Any wool",
+	wood = S"Any wood planks",
+	sand = S"Any sand",
+	stick = S"Any stick",
+	stone = S"Any kind of stone block",
+	vessel = S"Any vessel",
+
+	["color_red,flower"] = S"Any red flower",
+	["color_blue,flower"] = S"Any blue flower",
+	["color_black,flower"] = S"Any black flower",
+	["color_white,flower"] = S"Any white flower",
+	["color_green,flower"] = S"Any green flower",
+	["color_orange,flower"] = S"Any orange flower",
+	["color_yellow,flower"] = S"Any yellow flower",
+	["color_violet,flower"] = S"Any violet flower",
+
+	["color_red,dye"] = S"Any red dye",
+	["color_blue,dye"] = S"Any blue dye",
+	["color_grey,dye"] = S"Any grey dye",
+	["color_pink,dye"] = S"Any pink dye",
+	["color_cyan,dye"] = S"Any cyan dye",
+	["color_black,dye"] = S"Any black dye",
+	["color_white,dye"] = S"Any white dye",
+	["color_brown,dye"] = S"Any brown dye",
+	["color_green,dye"] = S"Any green dye",
+	["color_orange,dye"] = S"Any orange dye",
+	["color_yellow,dye"] = S"Any yellow dye",
+	["color_violet,dye"] = S"Any violet dye",
+	["color_magenta,dye"] = S"Any magenta dye",
+	["color_dark_grey,dye"] = S"Any dark grey dye",
+	["color_dark_green,dye"] = S"Any dark green dye",
+}
+
 local function err(str)
 	return log("error", str)
 end
@@ -672,15 +707,20 @@ local function get_tooltip(name, info)
 	local tooltip
 
 	if info.groups then
-		local groupstr, c = {}, 0
+		sort(info.groups)
+		tooltip = GROUP_NAMES[concat(info.groups, ",")]
 
-		for i = 1, #info.groups do
-			c = c + 1
-			groupstr[c] = clr("#ff0", info.groups[i])
+		if not tooltip then
+			local groupstr, c = {}, 0
+
+			for i = 1, #info.groups do
+				c = c + 1
+				groupstr[c] = clr("#ff0", info.groups[i])
+			end
+
+			groupstr = concat(groupstr, ", ")
+			tooltip = S("Any item belonging to the group(s): @1", groupstr)
 		end
-
-		groupstr = concat(groupstr, ", ")
-		tooltip = S("Any item belonging to the group(s): @1", groupstr)
 	else
 		tooltip = get_desc(name)
 	end
